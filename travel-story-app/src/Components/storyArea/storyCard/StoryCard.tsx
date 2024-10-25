6ef0e0818893d3716dc510247e7f795fd2cd88bf
ShirYahav <shiryahav07@gmail.com>
2024-10-25 12:51:15 +0100

    fixes img display in img card

--- travel-story-app/src/Components/storyArea/storyCard/StoryCard.tsx
+++ travel-story-app/src/Components/storyArea/storyCard/StoryCard.tsx
@@ -3,6 +3,8 @@ import './StoryCard.css';
 import StoryModel from '../../../Models/StoryModel';
 import { useNavigate } from 'react-router-dom';
 import defaultStoryImg from '../../../Assets/defaults/default-story-img.jpg';
+import axios from 'axios';
+import config from '../../../Utils/Config';
 
 interface StoryCardProps {
   story: StoryModel;
@@ -13,20 +15,27 @@ const StoryCard: React.FC<StoryCardProps> = ({ story }) => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchFirstPhoto = () => {
+    const fetchFirstPhoto = async () => {
       try {
         const firstPhoto = story?.locations?.[0]?.photos?.[0];
+        if (firstPhoto) {
+          const fileName = firstPhoto.toString().replace("photos/", "");
+          
+          const response = await axios.get(config.getPhotoByImgNameUrl + fileName, {
+            responseType: 'blob', 
+          });
 
-        if (!firstPhoto) {
-          setImageUrl(defaultStoryImg); 
-          return;
+          const reader = new FileReader();
+          reader.onloadend = () => {
+            setImageUrl(reader.result); 
+          };
+          reader.readAsDataURL(response.data);
+        } else {
+          setImageUrl(defaultStoryImg);
         }
-
-        setImageUrl(firstPhoto);
-
       } catch (error) {
         console.error("Error fetching the image:", error);
-        setImageUrl(defaultStoryImg);
+        setImageUrl(defaultStoryImg); 
       }
     };
 
@@ -53,6 +62,6 @@ const StoryCard: React.FC<StoryCardProps> = ({ story }) => {
       </div>
     </>
   );
-}
+};
 
 export default StoryCard;
